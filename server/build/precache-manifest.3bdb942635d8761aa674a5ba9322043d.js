d1f0af5a3c7fc4e9628a70e3c2acdeb4cb8d3891
Kumar <kumar.r@contus.in>
2019-12-30 12:36:08 +0530

    Added the react build into server

---
+++ server/build/precache-manifest.3bdb942635d8761aa674a5ba9322043d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1acbdda90b6aa4955942b1b632b7b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "020a8b8e8af27b4fefa8",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "2c8ab5b146369c6ff602",
+    "url": "/static/css/main.c1f75a53.chunk.css"
+  },
+  {
+    "revision": "020a8b8e8af27b4fefa8",
+    "url": "/static/js/2.1512fa7d.chunk.js"
+  },
+  {
+    "revision": "2c8ab5b146369c6ff602",
+    "url": "/static/js/main.1365dea4.chunk.js"
+  },
+  {
+    "revision": "35a7acf4fb928453b799",
+    "url": "/static/js/runtime-main.8727d782.js"
+  },
+  {
+    "revision": "84d761c998c28c8c9ffbfe12d1b0cdca",
+    "url": "/static/media/Kumar-Resume.84d761c9.pdf"
+  },
+  {
+    "revision": "36c666a596235e9dd9f544e2ca32c2f1",
+    "url": "/static/media/download-resume.36c666a5.png"
+  },
+  {
+    "revision": "c3f70e41b53c6194fe2d0f21a7ebf463",
+    "url": "/static/media/kumar_r_photo.c3f70e41.JPG"
+  }
+]);
